4a925a4363cca9091ea229e5149a0e3ef4f9da17
zAck <118198354+ZackMount@users.noreply.github.com>
2023-05-04 16:37:42 +0800

    Update InvitationGenerator.cs

--- App/InvitationGenerator.cs
+++ App/InvitationGenerator.cs
@@ -1,25 +1,24 @@
-﻿using ImageMagick;
+using ImageMagick;
 using Invitation_Generator;
 
 namespace NSSCTF.InvitationGenerator
 {
     public class Poster
     {
-        private string Username;
+        private string username;
 
-        private string AvatarPath;
-        private byte[] Avatar;
+        private byte[] avatar;
 
         private PosterStyle posterStyle;
-        public Poster(PosterStyle posterStyle, string Username, string AvatarPath)
+        public Poster(PosterStyle posterStyle, string username, string avatarPath)
         {
             this.posterStyle = posterStyle;
-            this.Username = Username;
-            this.Avatar = posterStyle.AvatarStyle switch
+            this.username = username;
+            this.avatar = posterStyle.AvatarStyle switch
             {
-                AvatarStyleType.Cirlce => FixAvatar(AvatarPath, Resource.T_circle),
-                AvatarStyleType.Fillet => FixAvatar(AvatarPath, Resource.T_fillet),
-                _ => File.ReadAllBytes(AvatarPath),
+                AvatarStyleType.Cirlce => FixAvatar(avatarPath, Resource.T_circle),
+                AvatarStyleType.Fillet => FixAvatar(avatarPath, Resource.T_fillet),
+                _ => File.ReadAllBytes(avatarPath),
             };
         }
         /// <summary>
@@ -28,47 +27,76 @@ namespace NSSCTF.InvitationGenerator
         /// <returns></returns>
         public Stream Generate()
         {
-            if (posterStyle is null) throw new ArgumentNullException("Poster Style");
-            var Result = new MemoryStream();
-            using (var Image = new MagickImage(posterStyle.Poster))
+            if (posterStyle is null) throw new ArgumentNullException(nameof(posterStyle), "Poster Style is null.");
+
+            var resultStream = new MemoryStream();
+
+            using (var image = new MagickImage(posterStyle.Poster))
             {
-                var drawUsername = new Drawables()
-                    .Font(posterStyle.Font)
-                    .FontPointSize(posterStyle.FontSize)
-                    .FillColor(new MagickColor(posterStyle.FontColor))
-                    .Gravity((ImageMagick.Gravity)posterStyle.Gravity)
-                    .Text(posterStyle.UsernamePosition.X, posterStyle.UsernamePosition.Y, Username)
-                    .Draw(Image);
-                using (var drawAvatar = new MagickImage(Avatar))
-                {
-                    drawAvatar.Resize(new MagickGeometry(posterStyle.AvatarSize.Width, posterStyle.AvatarSize.Height));
-                    Image.Composite(drawAvatar, posterStyle.AvatarPosition.X, posterStyle.AvatarPosition.Y, CompositeOperator.Over);
-                }
-                Image.Write(Result, MagickFormat.Jpg);
-                Image.Dispose();
+                DrawUsernameOnImage(image);
+                DrawAvatarOnImage(image);
+
+                image.Write(resultStream, MagickFormat.Jpg);
             }
-            return Result;
+
+            resultStream.Position = 0; //将流位置重置为 0
+            return resultStream;
         }
         /// <summary>
-        /// 按样式裁切头像
+        /// 绘制用户名
         /// </summary>
-        /// <param name="Avatar"></param>
-        /// <param name="Template"></param>
-        /// <returns></returns>
-        private byte[] FixAvatar(string AvatarPath, byte[] Template)
+        /// <param name="image"></param>
+        private void DrawUsernameOnImage(MagickImage image)
         {
-            var Result = new MemoryStream();
-            using (var Image = new MagickImage(Template))
-            {
-                Image.Resize(new MagickGeometry(posterStyle.AvatarSize.Width, posterStyle.AvatarSize.Height));
-                using (var drawAvatar = new MagickImage(AvatarPath))
-                {
-                    drawAvatar.Resize(new MagickGeometry(posterStyle.AvatarSize.Width, posterStyle.AvatarSize.Height));
-                    Image.Composite(drawAvatar, (ImageMagick.Gravity)posterStyle.Gravity, 0, 0, CompositeOperator.Atop);
-                }
-                Image.Write(Result);
-            }
-            return Result.ToArray();
+            new Drawables()
+                .Font(posterStyle.Font)
+                .FontPointSize(posterStyle.FontSize)
+                .FillColor(new MagickColor(posterStyle.FontColor))
+                .Gravity((ImageMagick.Gravity)posterStyle.Gravity)
+                .Text(posterStyle.UsernamePosition.X, posterStyle.UsernamePosition.Y, username)
+                .Draw(image);
         }
+        /// <summary>
+        /// 绘制头像
+        /// </summary>
+        /// <param name="image"></param>
+        private void DrawAvatarOnImage(MagickImage image)
+        {
+            using (var drawAvatar = new MagickImage(avatar))
+            {
+                drawAvatar.Resize(new MagickGeometry(posterStyle.AvatarSize.Width, posterStyle.AvatarSize.Height));
+                image.Composite(drawAvatar, posterStyle.AvatarPosition.X, posterStyle.AvatarPosition.Y, CompositeOperator.Over);
+            }
+        }
+
+        /// <summary>
+        /// Resizes and crops the avatar according to the style.
+        /// </summary>
+        /// <param name="avatarPath">The path of the avatar image file.</param>
+        /// <param name="template">The template image as a byte array.</param>
+        /// <returns>The resized and cropped avatar as a byte array.</returns>
+        private byte[] FixAvatar(string avatarPath, byte[] template)
+        {
+            using var templateImage = new MagickImage(template);
+            using var avatarImage = new MagickImage(avatarPath);
+            using var resultStream = new MemoryStream();
+
+            ResizeAndCropAvatar(templateImage, avatarImage);
+
+            templateImage.Write(resultStream);
+
+            return resultStream.ToArray();
+        }
+
+        private void ResizeAndCropAvatar(MagickImage templateImage, MagickImage avatarImage)
+        {
+            var avatarSize = new MagickGeometry(posterStyle.AvatarSize.Width, posterStyle.AvatarSize.Height);
+
+            templateImage.Resize(avatarSize);
+            avatarImage.Resize(avatarSize);
+
+            templateImage.Composite(avatarImage, ImageMagick.Gravity.Center, 0, 0, CompositeOperator.Atop);
+        }
+
     }
 }
